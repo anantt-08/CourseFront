99bf6a698ab6a3053a706f0500b727717d83b144
anant gupta <anantgupta888@gmail.com>
2021-01-28 10:01:16 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from "./store/index";
 import { Provider } from "react-redux";
+
 import 'react-notifications/lib/notifications.css';
 import 'mdbreact/dist/css/mdb.css';
 import "bootstrap/dist/css/bootstrap.min.css";
